864f51fb7f3df3b019aa638580abd0a832b2a9c9
rahulb1218 <rahulb1218@gmail.com>
2022-01-26 12:03:26 -0800

    suggestion in progress

--- spotify-test.py
+++ spotify-test.py
@@ -12,100 +12,31 @@ username = sys.argv[1]
 scope = 'user-read-private user-read-playback-state user-modify-playback-state'
 
 try:
-    token = util.prompt_for_user_token(username, scope)
+	token = util.prompt_for_user_token(username, scope)
 except (AttributeError, JSONDecodeError):
-    os.remove(f".cache-{username}")
-    token = util.prompt_for_user_token(username, scope)
+	os.remove(f".cache-{username}")
+	token = util.prompt_for_user_token(username, scope)
 
 # Create Spotify object
 spotifyObject = spotipy.Spotify(auth=token)
 
 devices = spotifyObject.devices()
-print(json.dumps(devices, sort_keys=True, indent=4))
 for i in range(len(devices['devices'])):
 	if  devices['devices'][i]['name'] == "iPad":
 		deviceID = devices['devices'][i]['id']
 
-# Get track information
-track = spotifyObject.current_user_playing_track()
-print(json.dumps(track, sort_keys=True, indent=4))
-print()
-artist = track['item']['artists'][0]['name']
-track = track['item']['name']
-
-if artist !="":
-    print("Currently playing " + artist + " - " + track)
-
-# User information
-user = spotifyObject.current_user()
-displayName = user['display_name']
-follower = user['followers']['total']
-
 while True:
+	print()
+	searchQuery = input("What song would you like to play? ")
+	print()
+	myTrack = spotifyObject.search(searchQuery, limit=3)
+	myTracks = []
 
-    print()
-    print(">>> Welcome to Spotify " + displayName + " :)")
-    print(">>> You have " + str(follower) + " followers.")
-    print()
-    print("0 - Search for an artist")
-    print("1 - exit")
-    print()
-    choice = input("Enter your choice: ")
-    # Search for artist
-    if choice == "0":
-        print()
-        searchQuery = input("Ok, what's their name?:")
-        print()
-        # Get search results
-        searchResults = spotifyObject.search(searchQuery,1,0,"artist")
-        # Print artist details
-        #artist = searchResults['artists']['items'][0]
-        myTrack = spotifyObject.search(searchQuery, limit=3)
-        myTracks = []
-     
-        for i in range(len(myTrack['tracks']['items'])):
-            print(str(i) + ": " + myTrack['tracks']['items'][i]['album']['name'])
-            print(myTrack['tracks']['items'][i]['album']['artists'][0]['name'])
-            myTracks.append(myTrack['tracks']['items'][i]['uri'])
-        print (myTracks)
-        songSelection = input("Enter a song number to see the album art: ")
-        trackSelectionList = []
-        trackSelectionList.append(myTracks[(int(songSelection))])
-        spotifyObject.start_playback(deviceID, None, trackSelectionList)
-       
-        """artistID = artist['id']
-         # Album details
-        trackURIs = []
-        trackArt = []
-        z = 0
-        # Extract data from album
-        albumResults = spotifyObject.artist_albums(artistID)
-        albumResults = albumResults['items']"""
 
-        """for item in albumResults:
-            print("ALBUM: " + item['name'])
-            albumID = item['id']
-            albumArt = item['images'][0]['url']
-
-            # Extract track data
-            trackResults = spotifyObject.album_tracks(albumID)
-            trackResults = trackResults['items']
-
-            for item in trackResults:
-                print(str(z) + ": " + item['name'])
-                trackURIs.append(item['uri'])
-                trackArt.append(albumArt)
-                z+=1
-            print()
-                # See album art
-            while True:
-                songSelection = input("Enter a song number to see the album art: ")
-                if songSelection == "x":
-                    break
-                trackSelectionList = []
-                trackSelectionList.append(trackURIs[int(songSelection)])
-                spotifyObject.start_playback(deviceID, None, trackSelectionList)"""
-              
-    # End program
-    if choice == "1":
-        break
+	for i in range(len(myTrack['tracks']['items'])):
+		print(str(i) + ": " + myTrack['tracks']['items'][i]['album']['name'] + " by " + myTrack['tracks']['items'][i]['album']['artists'][0]['name'])
+		myTracks.append(myTrack['tracks']['items'][i]['uri'])
+	songSelection = input("Is it song 0, 1, or 2? ")
+	trackSelectionList = []
+	trackSelectionList.append(myTracks[(int(songSelection))])
+	spotifyObject.start_playback(deviceID, None, trackSelectionList)
--- test.py
+++ test.py
@@ -1,18 +1,77 @@
 import discord
 import os
-
+import sys
+import json
+import spotipy
+import webbrowser
+import spotipy.util as util
+from json.decoder import JSONDecodeError
 client = discord.Client()
+myTrackNames = []
+myTracks = []
+suggested = []
+suggestedNames = []
+username = "Rahul1218"
+scope = 'user-read-private user-read-playback-state user-modify-playback-state'
+
+try:
+	token = util.prompt_for_user_token(username, scope)
+except (AttributeError, JSONDecodeError):
+	os.remove(f".cache-{username}")
+	token = util.prompt_for_user_token(username, scope)
+
+# Create Spotify object
+spotifyObject = spotipy.Spotify(auth=token)
+
+devices = spotifyObject.devices()
+for i in range(len(devices['devices'])):
+	if  devices['devices'][i]['name'] == "iPad":
+		deviceID = devices['devices'][i]['id']
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+	print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+	
+	if message.author == client.user:
+		return
+	
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('fuck me daddy ;)')
+	if message.content.startswith('suggest'):
+		myTracks.clear()
+		myTrackNames.clear()
+		await message.channel.send("What song would you like to play? ")
+		print(message.content.split(" ", 1)[1])
+		searchQuery = message.content.split(" ", 1)[1]
+		myTrack = spotifyObject.search(searchQuery, limit=3)
+		
+		for i in range(len(myTrack['tracks']['items'])):
+			await message.channel.send(str(i) + ": " + myTrack['tracks']['items'][i]['album']['name'] + " by " + myTrack['tracks']['items'][i]['album']['artists'][0]['name'])
+			myTracks.append(myTrack['tracks']['items'][i]['uri'])
+			myTrackNames.append(myTrack['tracks']['items'][i]['album']['name'] + " by " + myTrack['tracks']['items'][i]['album']['artists'][0]['name'])
+		await message.channel.send("Is it song 0, 1, or 2? ")
+		
+	if message.content == "0" or message.content == "1" or message.content == "2":
+		songSelection = message.content
+		trackSelectionList = []
+		print(songSelection)
+		trackSelectionList.append(myTracks[(int(songSelection))])
+		print(trackSelectionList)
+		#spotifyObject.start_playback(deviceID, None, trackSelectionList)
+		if len(suggestedNames) < 4:
+			suggested.append(myTracks[(int(songSelection))])
+			suggestedNames.append(myTrackNames[(int(songSelection))])
+			if len(suggestedNames) == 3:
+				await message.channel.send("Please vote on next track.")
+				print(suggestedNames)
+		else:
+			await message.channel.send("Suggestions are full, please wait for next track.")
+		spotifyObject.add_to_queue(myTracks[(int(songSelection))], None)
+		myTracks.clear()
+		myTrackNames.clear()
+	if message.content == "pause":
+		spotifyObject.stop_playback(deviceID, None)
 
 client.run("OTM1NzU2NzYzNjA3NzM2MzQw.YfDRZQ.z_5tDyTJVWMOEn1TKtf-H4Qu8LQ")
